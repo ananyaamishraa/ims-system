6c4195323ede37ca7ccf582c5491ebfd88446544
Ananya Mishra <ananyamishra0403@gmail.com>
2026-04-30 18:40:41 +0530

    Update Redis connection settings in queue_config.py

--- queue_config.py
+++ queue_config.py
@@ -1,5 +1,11 @@
 from redis import Redis
 from rq import Queue
 
-redis_conn = Redis(host="localhost", port=6379)
-queue = Queue(connection=redis_conn) 
+redis_conn = Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True,
+    health_check_interval=30
+)
+
+queue = Queue("default", connection=redis_conn) 
