56d8649b3bddef6acab04a2245522910bff4218a
Ananya Mishra <ananyamishra0403@gmail.com>
2026-04-30 18:38:46 +0530

    Refactor database initialization into init function
    Encapsulate database initialization in a function and add success message.

--- init_db.py
+++ init_db.py
@@ -1,4 +1,10 @@
 from db_config import engine, Base
 from models import Incident
 
-Base.metadata.create_all(bind=engine) 
+# Creates all tables safely
+def init():
+    Base.metadata.create_all(bind=engine)
+    print("✅ Database initialized successfully")
+
+if __name__ == "__main__":
+    init()
